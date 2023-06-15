e7a07feb3fef4600d0599e3d46112a7623babfc3
DESKTOP-A45213N\Asad <m.asadh@outlook.com>
2023-06-15 01:29:47 -0400

    work

--- data_loaders/humanml/utils/get_opt.py
+++ data_loaders/humanml/utils/get_opt.py
@@ -73,7 +73,7 @@ def get_opt(opt_path, device):
         opt.motion_dir = pjoin(opt.data_root, 'features')
         opt.text_dir = pjoin(opt.data_root, 'texts')
         opt.joints_num = 42
-        opt.dim_pose = 126
+        opt.dim_pose = 1662
         opt.max_motion_length = 250
     else:
         raise KeyError('Dataset not recognized')
