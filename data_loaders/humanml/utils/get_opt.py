f5d9d5c63eb4641de7014ecdf903328282be1d37
DESKTOP-A45213N\Asad <m.asadh@outlook.com>
2023-07-15 18:39:34 -0400

    no face base model

--- data_loaders/humanml/utils/get_opt.py
+++ data_loaders/humanml/utils/get_opt.py
@@ -73,8 +73,8 @@ def get_opt(opt_path, device):
         opt.motion_dir = pjoin(opt.data_root, 'features')
         opt.text_dir = pjoin(opt.data_root, 'texts')
         opt.joints_num = 21
-        opt.dim_pose = 63
-        opt.max_motion_length = 150
+        opt.dim_pose = 258
+        opt.max_motion_length = 250
     else:
         raise KeyError('Dataset not recognized')
 
